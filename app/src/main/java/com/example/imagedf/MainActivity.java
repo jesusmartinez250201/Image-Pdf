b2772f726895bad6a5becc9c8c8beb8fc96e4e94
jesusmartinez250201 <1930337@upv.edu.mx>
2022-10-11 11:30:11 -0500

    Metodo para crear pdf y seleccion de directorio

--- app/src/main/java/com/example/imagedf/MainActivity.java
+++ app/src/main/java/com/example/imagedf/MainActivity.java
@@ -3,10 +3,18 @@ package com.example.imagedf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +25,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -36,8 +55,10 @@ public class MainActivity extends AppCompatActivity {
     EditText txtNombrePdf;
     Button btnSelectImages;
     Button btnCreatePdf;
+    Button btnCambiarDirectorio;
     private static final String directorioInicial = Environment.getExternalStorageDirectory().toString();
     private static final String nombreDirectorio = "PDFs";
+    private static String directorioActual = directorioInicial + File.separator + nombreDirectorio;
     ArrayList<Uri> listaImagenes = new ArrayList<>();
     ArrayList<String> listaRutasImagenes = new ArrayList<>();
     TextView txtVerRutas;
@@ -59,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
         btnSelectImages = findViewById(R.id.btnSelectImages);
         btnCreatePdf = findViewById(R.id.btnCreatePdf);
         txtVerRutas = findViewById(R.id.txtVerRutas);
+        btnCambiarDirectorio = findViewById(R.id.btnCambiarDirectorio);
 
         btnCreatePdf.setOnClickListener((view) -> {
             String nombrePdf = txtNombrePdf.getText().toString();
@@ -83,58 +105,82 @@ public class MainActivity extends AppCompatActivity {
                                     long arg3) {
             }
         });
+
+        //LISTENER PARA CAMBIAR DIRECTORIO
+        btnCambiarDirectorio.setOnClickListener((view) -> {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP){
+                Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+                i.addCategory(Intent.CATEGORY_DEFAULT);
+                startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999);
+            }
+
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-            if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
-            if (data.getClipData() != null) {
-                //CUANDO SE SELECCIONAN VARIAS IMAGENES
-                //PRUEBA: vaciar el textView
-                txtVerRutas.setText("");
-                array.clear();
-                adapter.notifyDataSetChanged();
-                listaImagenes.clear();
-                listaRutasImagenes.clear();
-                int imagenesSeleccionadas = data.getClipData().getItemCount();
-                //AGREGAR LAS IMAGENES A LA LISTA
-                for (int i = 0; i < imagenesSeleccionadas; i++) {
-                    Uri imagenUri = data.getClipData().getItemAt(i).getUri();
-                    listaImagenes.add(imagenUri);
-                    array.add(imagenUri.getPath());
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK && data != null) {
+                    if (data.getClipData() != null) {
+                        //CUANDO SE SELECCIONAN VARIAS IMAGENES
+                        //PRUEBA: vaciar el textView
+                        txtVerRutas.setText("");
+                        array.clear();
+                        adapter.notifyDataSetChanged();
+                        listaImagenes.clear();
+                        listaRutasImagenes.clear();
+                        int imagenesSeleccionadas = data.getClipData().getItemCount();
+                        //AGREGAR LAS IMAGENES A LA LISTA
+                        for (int i = 0; i < imagenesSeleccionadas; i++) {
+                            Uri imagenUri = data.getClipData().getItemAt(i).getUri();
+                            listaImagenes.add(imagenUri);
+                            array.add(imagenUri.getPath());
 
-                    listaRutasImagenes.add(imagenUri.getPath());
-                    //Toast.makeText(this, "Ruta: " + imagenUri.getPath(), Toast.LENGTH_SHORT).show();
+                            listaRutasImagenes.add(imagenUri.getPath());
+                            //Toast.makeText(this, "Ruta: " + imagenUri.getPath(), Toast.LENGTH_SHORT).show();
+                        }
+                        //esto termina de actualizar el listView
+                        adapter.notifyDataSetChanged();
+                        //PRUEBA: Ver rutas de las imagenes seleccionadas
+                        for (String ruta : listaRutasImagenes) {
+
+                            txtVerRutas.append(ruta + "\n");
+                            //array.add(ruta);
+
+                        }
+
+                    } else if (data.getData() != null) {
+                        //SI SOLO SELECCIONA UNA IMAGEN
+                        //PRUEBA: Vaciar el TextView
+                        txtVerRutas.setText("");
+                        listaImagenes.clear();
+                        array.clear();
+                        //esto termina de actualizar el listView
+                        adapter.notifyDataSetChanged();
+                        listaRutasImagenes.clear();
+                        Uri imagenUri = data.getData();
+                        listaImagenes.add(imagenUri);
+                        listaRutasImagenes.add(imagenUri.getPath());
+                        array.add(imagenUri.getPath());
+                        //esto termina de actualizar el listView
+                        adapter.notifyDataSetChanged();
+                        txtVerRutas.setText(listaRutasImagenes.get(0));
+                    }
+
                 }
-                //esto termina de actualizar el listView
-                adapter.notifyDataSetChanged();
-                //PRUEBA: Ver rutas de las imagenes seleccionadas
-                for (String ruta : listaRutasImagenes) {
-
-                    txtVerRutas.append(ruta + "\n");
-                    //array.add(ruta);
-
+                break;
+            case 9999:
+                if (resultCode == RESULT_OK && data != null) {
+                    Uri uri = data.getData();
+                    String path = uri.getPath();
+                    String[] split = path.split(":");
+                    String newPath = split[1];
+                    directorioActual = directorioInicial + File.separator + newPath;
+                    Toast.makeText(this, "Directorio actual: " + directorioActual, Toast.LENGTH_SHORT).show();
                 }
-
-            } else if (data.getData() != null) {
-                //SI SOLO SELECCIONA UNA IMAGEN
-                //PRUEBA: Vaciar el TextView
-                txtVerRutas.setText("");
-                listaImagenes.clear();
-                array.clear();
-                //esto termina de actualizar el listView
-                adapter.notifyDataSetChanged();
-                listaRutasImagenes.clear();
-                Uri imagenUri = data.getData();
-                listaImagenes.add(imagenUri);
-                listaRutasImagenes.add(imagenUri.getPath());
-                array.add(imagenUri.getPath());
-                //esto termina de actualizar el listView
-                adapter.notifyDataSetChanged();
-                txtVerRutas.setText(listaRutasImagenes.get(0));
-            }
-
+                break;
         }
 
     }
@@ -156,21 +202,47 @@ public class MainActivity extends AppCompatActivity {
      */
     private void createPdf() {
         String nombrePdf = txtNombrePdf.getText().toString();
-        File directorio = new File(directorioInicial + File.separator + nombreDirectorio);
+        File directorio = new File(directorioActual);
         if (!directorio.exists()) {
-            directorio.mkdirs();
+            directorio.mkdir();
         }
         File pdf = new File(directorio, nombrePdf + ".pdf");
         if (pdf.exists()) {
             txtNombrePdf.setError("El nombre del pdf ya existe");
             txtNombrePdf.requestFocus();
         } else {
+            File f = new File(directorioActual, nombrePdf + ".pdf");
+            FileOutputStream ficheroPdf = null;
+            try {
+                ficheroPdf = new FileOutputStream(f);
+                Document documento = new Document();
+                PdfWriter.getInstance(documento, ficheroPdf);
+                documento.open();
+                for (int i = 0; i < listaImagenes.size(); i++) {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), listaImagenes.get(i));
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                    Image imagen = Image.getInstance(stream.toByteArray());
+                    imagen.scaleToFit(PageSize.A4.getWidth() - 20, PageSize.A4.getHeight() - 20);
+                    imagen.setAlignment(Image.ALIGN_CENTER);
+                    documento.add(imagen);
+                    documento.newPage();
+                }
+                documento.close();
+                Toast.makeText(this, "Pdf creado correctamente", Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
 
 
-//ESto es pa que jale el mover
+    //ESto es pa que jale el mover
     private TouchListView.DropListener onDrop=new TouchListView.DropListener() {
         @Override
         public void drop(int from, int to) {
